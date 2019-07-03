9e8530ad1fb29aa3e93f4fb972b65d5381fb14e1
Saad's Surface <saad.f.alazem@gmail.com>
2019-07-03 10:17:40 -0400

    updated

--- designs.js
+++ designs.js
@@ -1,25 +1,30 @@
 let gitSize = document.getElementById("sizePicker");
 let gitColor = document.getElementById("colorPicker");
 let gitCanvas = document.getElementById("pixelCanvas");
-let height = document.getElementById("inputHeight").value;
-let width = document.getElementById("inputWidth").value;
+let inputHeight = document.getElementById("inputHeight");
+let inputWidth = document.getElementById("inputWidth");
 
+/* This function takes the value from the user,
+assigns it to the height and width variables,
+runs the makeGrid function and then reset every thing */
 function sizing() {
     gitSize.addEventListener("submit", function() {
         document.getElementById("pixelCanvas").innerHTML = "";
-        let height = document.getElementById("inputHeight").value;
-        let width = document.getElementById("inputWidth").value;
+        let height = inputHeight.value;
+        let width = inputWidth.value;
         makeGrid(height, width);
-        let resetHeight = document.getElementById("inputHeight").value = "";
-        let resetWidth = document.getElementById("inputWidth").value = "";
+        let height2 = inputHeight.value  = "";
+        let width2 = inputWidth.value = "";
     });
 }
 
-function makeGrid(height, width) {
-    for (let c = 1; c <= height; c++) {
+/* This function create columns and rows then apply style 
+(backgroundColor) to every indvidual cell */
+function makeGrid(inputHeight, inputWidth) {
+    for (let c = 1; c <= inputHeight; c++) {
         let addColumn = document.createElement("tr");
         gitCanvas.appendChild(addColumn);
-        for (let r = 1; r <= width; r++) {
+        for (let r = 1; r <= inputWidth; r++) {
             let addCell = document.createElement("td");
             addColumn.appendChild(addCell);
             addCell.addEventListener("click", function() {
